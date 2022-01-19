2be6d402d0499f0249c7756155adbe281c2ae18a
gary318 <gary318@126.com>
2022-01-19 15:52:30 +0800

    Primitive path-finding

--- assess.py
+++ assess.py
@@ -1,9 +1,6 @@
 from adk import *
 
-# written by lbr
-
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+ACT = ((1,0),(0,1),(-1,0),(0,-1));
 INF = 100000000;
 # constants
 
@@ -12,234 +9,128 @@ SEARCH_LIMIT = 100
 # tunable parameters
 
 
-class AI:
+
+class assess:
     """
-    This AI proceeds according to following rules:
-
-    The first snake is active, all other snakes split by the first snake are passive.
-
-    An active snake will act according to self.active_strategy.
-
-    A passive snake will act according to self.passive_strategy.
-
-    Check those functions for detail.
+    估价模块
     """
     ctx : Context;
-    snake : Snake;
-    order : "dict[int,tuple[int,int]]";#不完全确定
+    snakes : "list[Snake]";
+    game_map : Map;
+    x_leng : int;
+    y_leng : int;
+    snkid : int;
 
-    def __init__(self):
-        self.ctx = None
-        self.snake = None
-        self.order = dict()
-
-    def check(self, op : int):
-        """
-        :param op: the direction
-        :return: True if this move is legal and will not result in solidification.
-                 False otherwise.
-        """
-        x = self.snake.coor_list[0][0] + dx[op]
-        y = self.snake.coor_list[0][1] + dy[op]
-        if x < 0 or y < 0 or x >= 16 or y >= 16 or self.ctx.game_map.wall_map[x][y] != -1:
-            return False
-        # check wall and out of bounds
-        if self.snake.get_len() > 1 and x == self.snake.coor_list[1][0] and y == self.snake.coor_list[1][1]:
-            return False
-        # check turn back
-        if self.ctx.game_map.snake_map[x][y] != -1:
-            return False
-        # check other snake
-        return True
-
-    def check_self(self, op : int):
-        """
-        :param op: the direction
-        :return: True if this move is legal, could possibly result in solidification.
-                 False otherwise.
-        """
-        x = self.snake.coor_list[0][0] + dx[op]
-        y = self.snake.coor_list[0][1] + dy[op]
-        if x < 0 or y < 0 or x >= 16 or y >= 16 or self.ctx.game_map.wall_map[x][y] != -1:
-            return False
-        if self.snake.get_len() > 1 and x == self.snake.coor_list[1][0] and y == self.snake.coor_list[1][1]:
-            return False
-        if self.ctx.game_map.snake_map[x][y] != -1 and self.ctx.game_map.snake_map[x][y] != self.snake.id:
-            return False
-        return True
-
-    def closest_food_strategy(self):
-        """
-        Search for the closest food and to go that direction, if legal.
-
-        :return: the chosen direction
-        """
-        def calc_dist(x1 : int, y1 : int, x2 : int, y2 : int) -> "tuple[int,int]":
-            """
-            Calculate the Manhattan distance between snake head and item.
-            And find the possible direction to the item, at most 2.
-
-            :return: (dist, possible_direction)
-            """
-            pos = []
-            if x1 > x2:
-                pos.append(0)
-            if x1 < x2:
-                pos.append(2)
-            if y1 > y2:
-                pos.append(1)
-            if y1 < y2:
-                pos.append(3)
-            return abs(x1 - x2) + abs(y1 - y2), pos
-
-        valid = []
-        for i in range(4):
-            if self.check(i):
-                valid.append(i)
-        if len(valid) == 0:
-            if self.snake.get_len() > 1 and self.snake.coor_list[0][0] + dx[0] == self.snake.coor_list[1][0] and self.snake.coor_list[0][1] + dy[0] == self.snake.coor_list[1][1]:
-                return 1
-            return 0
-        # calculate the legal moves without concerning the food
-
-        coor = self.snake.coor_list
-        dist, val = INF, []
-        for item in self.ctx.game_map.item_list:
-            if item.type != 0:
-                continue
-            # search food only
-
-            if item.time > self.ctx.turn + SEARCH_LIMIT or item.time + item.param < self.ctx.turn:
-                continue
-            # search valid food only
-
-            d, pos = calc_dist(item.x, item.y, coor[0][0], coor[0][1])
-            pos = [i for i in pos if i in valid]
-            # use legal move only
-
-            if len(pos) > 0 and d < dist and \
-                    d + self.ctx.turn <= item.time + item.param <= d + self.ctx.turn + SPLIT_LIMIT / 2:
-                dist, val = d, pos
-            # search reachable food only
-            # not that the span of a snake is at least SPLIT_LIMIT / 2
-
-        # chose the closest reachable food and use legal move
-
-        if len(val) == 0:
-            val = valid
-        i = random.randint(0, len(val) - 1)
-        # randomly chose one if multiple available
-
-        return val[i]
-
-    def active_strategy(self):
-        """
-        Strategy for the active snake. Split whenever limit is reached.
-        Fire whenever possible. Otherwise search for food.
-        """
-        if len(self.snake.coor_list) >= SPLIT_LIMIT:
-            return 6
-        elif len(self.snake.item_list) > 0 and len(self.snake.coor_list) > 1:
-            return 5
-        else:
-            return self.closest_food_strategy() + 1
-
-    def solidify_strategy(self):
-        """
-        Try to solidify self by forming a 2 * 2 area.
-        It requires at most 4 move to solidify.
-
-        :return: direction to move
-        """
-        if self.snake.id in self.order:
-            rk, order = self.order[self.snake.id]
-            self.order[self.snake.id] = rk + 1, order
-            return order[rk] % 4
-            # not the first move, follow the previous move
-        else:
-            for i in range(4):
-                if self.check_self(i):
-                    order = [i, i + 1, i + 2, i + 3]
-                    self.order.update({self.snake.id: (1, order)})
-                    return i % 4
-            # the first move, choose a legal direction at store it in self.order
-            return 0
-
-    def passive_strategy(self):
-        """
-        Strategy for other snake. Always solidify.
-        """
-        return self.solidify_strategy() + 1
-
-    def judge(self, snake : Snake, ctx : Context):
-        """
-        :param snake: current snake
-        :param ctx: current context
-        :return: the decision
-        """
+    def __init__(self,ctx : Context,snkid : int = -1):
         self.ctx = ctx;
-        self.snake = snake;
-        if snake.id == 0 or snake.id == 1:
-            return self.active_strategy();
+        self.game_map = ctx.get_map();
+        self.x_leng,self.y_leng = self.game_map.length,self.game_map.width;
+        self.snakes,self.snkid = ctx.snake_list,snkid;
+    
+    def calc_spd_map(self,snkid : int) -> "list[list[int]]":
+        '''
+        计算“速度势力图”，即哪方能先到达指定格
+        返回一个二维数组，其中:
+            +1表示我方在普通环境下先到达
+            +2表示通过使用射线，我方先到达
+        '''
+        FLAG_NORM_FIRST = 1;
+        FLAG_RAY_FIRST = 2;
+        tmp = [[0 for y in range(self.y_leng)] for x in range(self.x_leng)];#(tmp)
+        ans = [[0 for y in range(self.y_leng)] for x in range(self.x_leng)];
+        
+        #把蛇占据的先填上
+        for sn in self.snakes:
+            sid = 1;
+            if sn.camp != self.ctx.current_player:
+                sid = -1;
+            for t in range(len(sn.coor_list)):
+                x,y = sn.coor_list[t];
+                tmp[x][y] = sid * (len(sn.coor_list)-t);
+        
+        vis = [[0 for y in range(self.y_leng)] for x in range(self.x_leng)];
+        queue : "list[tuple[int,int,int,int]]" = [];#(x,y,step,comp)
+        for sn in self.snakes:
+            queue.append((sn.coor_list[0][0],sn.coor_list[0][1],1,sn.camp == self.ctx.current_player));
+        
+        while len(queue):
+            x,y,step,comp = queue[0];
+            del queue[0];
+            vis[x][y] = 1;
+
+            for act in ACT:
+                tx,ty = x+act[0],y+act[1];
+                if not self.check_mov_norm(tx,ty,snkid):#这里不对
+                    continue;
+                #还没写完
+                
+    def find_path(self,snkid : int,tgtx : int,tgty: int) -> "tuple[int,int]":
+        """
+        寻路，从snkid所在位置到(tgtx,tgty)
+        这里认为所有蛇是静态的
+        返回(下一步走法,距离)
+        """
+        def rev_step(st : int) -> int:
+            st += 1;
+            return (-1,3,4,1,2)[st];
+        
+        nx,ny = self.snakes[snkid].coor_list[0];
+        if nx == tgtx and ny == tgty:#已经在终点
+            return (-1,0);
+
+        vis = [[0 for y in range(self.y_leng)] for x in range(self.x_leng)];
+        queue : "list[tuple[int,int,int]]" = [];#(x,y,step)
+
+        queue.append((tgtx,tgty,0));#从终点开始
+
+        while len(queue):
+            x,y,step = queue[0];
+            del queue[0];
+            vis[x][y] = 1;
+
+            for i,act in enumerate(ACT):
+                tx,ty = x+act[0],y+act[1];
+                if tx == nx and ty == ny:
+                    return (rev_step(i),step+1);
+                if not self.check_mov_norm(tx,ty,snkid):#认为所有蛇是静态的
+                    continue;
+                if not vis[tx][ty]:
+                    queue.append((tx,ty,step+1));
+        return (-1,-1);
+
+    def check_mov_norm(self,tx : int,ty : int,snkid : int) -> bool:
+        """
+        判断【立刻】走到(tx,ty)是否可行（不会被撞死）
+        """
+        #越界/撞墙
+        if tx < 0 or ty < 0 or tx >= 16 or ty >= 16 or self.ctx.game_map.wall_map[tx][ty] != -1:
+            return False;
+        #撞蛇
+        if self.snakes[snkid].get_len() == 2:
+            if self.ctx.game_map.snake_map[tx][ty] == snkid:
+                return True;
+            if self.ctx.game_map.snake_map[tx][ty] != -1:
+                return False;
+            return True;
         else:
-            return self.passive_strategy();
+            if self.ctx.game_map.snake_map[tx][ty] != -1:
+                return False;
+            return True;
 
+    # def check_self(self, op : int):
+    #     """
+    #     :param op: the direction
+    #     :return: True if this move is legal, could possibly result in solidification.
+    #              False otherwise.
+    #     """
+    #     x = self.snake.coor_list[0][0] + dx[op]
+    #     y = self.snake.coor_list[0][1] + dy[op]
+    #     if x < 0 or y < 0 or x >= 16 or y >= 16 or self.ctx.game_map.wall_map[x][y] != -1:
+    #         return False
+    #     if self.snake.get_len() > 1 and x == self.snake.coor_list[1][0] and y == self.snake.coor_list[1][1]:
+    #         return False
+    #     if self.ctx.game_map.snake_map[x][y] != -1 and self.ctx.game_map.snake_map[x][y] != self.snake.id:
+    #         return False
+    #     return True
 
-def run():
-    """
-    This function maintains the context, i.e. simulating the game.
-    It is not necessary to understand this function for you to write an AI.
-    """
-    c = Client()
-    # game config
-    (length, width, max_round, player) = c.fetch_data()
-    config = GameConfig(width=width, length=length, max_round=max_round)
-    ctx = Context(config=config)
-    current_player = 0
-
-    logging.info('Assigned player %d', player)
-
-    # read items
-    item_list = c.fetch_data()
-    ctx.game_map = Map(item_list, config=config)
-    controller = Controller(ctx)
-    # read & write operations
-    playing = True
-    ai = AI()
-    while playing:
-        if controller.ctx.turn > max_round:
-            res = c.fetch_data()
-            sys.stderr.write(str(res[1:]))
-            break
-        if current_player == 0:
-            controller.round_preprocess()
-        controller.round_init()
-        if player == current_player:  # Your Turn
-            while controller.next_snake != -1:
-                current_snake = controller.current_snake_list[controller.next_snake][0]
-                op = ai.judge(current_snake, controller.ctx)  # TODO: Complete the Judge Function
-                logging.debug(str(op))
-                if not controller.apply(op):
-                    pass
-                    # raise RuntimeError("Illegal Action!!!")
-                c.send_data(op)
-                res = c.fetch_data()
-                if res[0] == -1:
-                    playing = False
-                    sys.stderr.write(str(res[1:]))
-                    break
-            controller.next_player()
-        else:
-            while True:
-                if controller.next_snake == -1:
-                    controller.next_player()
-                    break
-                op = c.fetch_data()
-                if op[0] == -1:
-                    playing = False
-                    sys.stderr.write(str(op[1:]))
-                    break
-                controller.apply(op[0])
-        current_player = 1 - current_player
-    while True:
-        pass
+    
